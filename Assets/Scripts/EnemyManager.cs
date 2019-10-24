e933f057dc012951f59341789a0e8868dcde5cfe
Brown, Parker <parker.brown1305@gmail.com>
2019-10-23 21:22:08 -0500

    First Commit, EnemyManager and SpawnTrigger

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,61 @@
+﻿
+/*
+ * Author: Parker Brown
+ * Date Created: 10/2/19
+ * Purpose:  Controls the spawning of enemies
+ * Implementation:
+ *      1. Create an empty game object e.g "Enemy_01_Manager", attach this script
+ *      2. drag and drop enemy prefab into enemy field.  Create a new enemy manager for each type of enemy
+ *      3. set spawnpoint size and fill with spawn point gameobjects (uniquely name each in heiarchy)
+ *  See SpawnTriggerExample for how to start/stop spawning enemies from other scripts.
+ * Tutorial: https://learn.unity.com/tutorial/survival-shooter-training-day-phases?projectId=5c514921edbc2a002069465e#5c7f8528edbc2a002053b71f
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemy;    //prefab of enemy to be spawned
+
+    public float firstSpawn = 5f;     //time in seconds to first spawn
+    public float spawnTime = 3f;       //time in seconds between spawns
+    public Transform[] spawnPoints;    //where enemies spawn
+    public bool spawnEnabled;          //see implementation, controls spawner
+    public bool spawning;              //is object spawning enemies?
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnEnabled = false;
+        spawning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnEnabled && !spawning)
+        {
+            Debug.Log("INIT");
+            InvokeRepeating("Spawn", firstSpawn, spawnTime);
+            spawning = true;
+        }
+
+        if (!spawnEnabled && spawning)
+        {
+            CancelInvoke();
+            spawning = false;
+        }
+
+    }
+
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        Debug.Log("Just Spawned an Enemy");
+        Debug.Log(Time.fixedTime);
+
+    }
+
+}
---
+++ Assets/Scripts/SpawnTriggerExample.cs
@@ -0,0 +1,35 @@
+﻿
+/*
+ * Author: Parker Brown
+ * Date Created: 10/9/19
+ * Purpose:  An example to show how to spawn enemies using an enemy manager
+ * Implementation:
+ *      1. drag an enemy manager game object in the scene to a variable
+ *      2. access spawnEnabled as seen on line 32
+ *      
+ *      Note that this script is an example implementation ONLY.  How you 
+ *      trigger (or stop) spawns is up to you.
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTriggerExample : MonoBehaviour
+{
+    public GameObject EnemyManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Trigger());
+    }
+
+    IEnumerator Trigger()
+    {
+        yield return new WaitForSeconds(.2f);
+        Debug.Log("Set spawnEnabled = true");
+        EnemyManager.GetComponent<EnemyManager>().spawnEnabled = true;
+    }
+}
