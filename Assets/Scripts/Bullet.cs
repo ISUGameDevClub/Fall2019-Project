ae41e9621241d0dc61071c240089ccb2769f2e86
Reyes, Michael <mradianlogs@gmail.com>
2019-10-23 21:22:08 -0500

    Updating my branch.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+    public int speed;
+    //private Enemy enemy;
+    private Entity entity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit: " + collision.name + "!");
+        //entity = collision.GetComponent<Enemy>();
+        entity = collision.GetComponent<Player>();
+        if (entity!= null)
+        {
+            entity.GetComponent<HealthManager>().TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int roundNum;
+    private bool hasLost;
+
+    private List<Enemy> enemyManager;
+    private List<Livestock> livestockManager;
+    private List<Bullet> bulletManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public List<Enemy> getEnemyManager()
+    {
+        return enemyManager;
+    }
+    public void setEnemyManager(List<Enemy> newEnemyManager)
+    {
+        this.enemyManager = newEnemyManager;
+    }
+
+    public List<Livestock> getLivestockManager()
+    {
+        return livestockManager;
+    }
+    public void setLivestockManager(List<Livestock> newLivestockManager)
+    {
+        this.livestockManager = newLivestockManager;
+    }
+
+    public List<Bullet> getBulletManager()
+    {
+        return bulletManager;
+    }
+    public void setBulletManager(List<Bullet> newBulletManager)
+    {
+        this.bulletManager = newBulletManager;
+    }
+
+    public void checkIfLost()
+    {
+
+    }
+
+    public void checkIfWonRound()
+    {
+
+    }
+
+    public void wonRound()
+    {
+
+    }
+
+    public void lostGame()
+    {
+
+    }
+
+    public void incrementRound()
+    {
+
+    }
+
+    public void restartGame()
+    {
+
+    }
+}
